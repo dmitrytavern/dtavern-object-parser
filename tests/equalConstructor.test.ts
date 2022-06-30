605d34cd7ad1c3a932488ab50939351ba7bb59a2
Dmitry Tavern <dogger.work@gmail.com>
2022-07-01 01:38:21 +0300

    test: refactor tests and add new test for default

--- tests/equalConstructor.test.ts
+++ tests/equalConstructor.test.ts
@@ -1,7 +1,7 @@
 import { isEqualConstructor } from '../src/isEqualConstructor'
 
 describe('Check primitive constructors', () => {
-	it('Check string proto', () => {
+	it('Check string constructor', () => {
 		expect(isEqualConstructor('', String)).toBeTruthy()
 		expect(isEqualConstructor('hello world', String)).toBeTruthy()
 		expect(isEqualConstructor('123', String)).toBeTruthy()
@@ -11,13 +11,15 @@ describe('Check primitive constructors', () => {
 		expect(isEqualConstructor(Symbol('text'), String)).toBeFalsy()
 	})
 
-	it('Check number proto', () => {
+	it('Check number constructor', () => {
 		expect(isEqualConstructor(123, Number)).toBeTruthy()
+		expect(isEqualConstructor(NaN, Number)).toBeTruthy()
+		expect(isEqualConstructor(1324e3, Number)).toBeTruthy()
 
 		expect(isEqualConstructor('134', Number)).toBeFalsy()
 	})
 
-	it('Check boolean proto', () => {
+	it('Check boolean constructor', () => {
 		expect(isEqualConstructor(true, Boolean)).toBeTruthy()
 		expect(isEqualConstructor(false, Boolean)).toBeTruthy()
 
@@ -27,10 +29,22 @@ describe('Check primitive constructors', () => {
 		expect(isEqualConstructor(undefined, Boolean)).toBeFalsy()
 	})
 
-	it('Check symbol proto', () => {
+	it('Check symbol constructor', () => {
 		expect(isEqualConstructor(Symbol('test'), Symbol)).toBeTruthy()
 
 		expect(isEqualConstructor('Symbol', Symbol)).toBeFalsy()
+	})
+})
+
+describe('Check not primitive constructors', () => {
+	it('Check array constructor', () => {
+		expect(isEqualConstructor([], Array)).toBeTruthy()
+		expect(isEqualConstructor({}, Array)).toBeFalsy()
+	})
+
+	it('Check object constructor', () => {
+		expect(isEqualConstructor({}, Object)).toBeTruthy()
+		expect(isEqualConstructor([], Object)).toBeFalsy()
 	})
 })
 
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -4,137 +4,151 @@ it('Function properties exists', () => {
 	expect(() => defineOptions()).toThrow()
 	expect(() => defineOptions('hello', ['hello'])).toThrow()
 	expect(() => defineOptions(null, ['hello'])).toThrow()
+	expect(() => defineOptions(undefined, ['hello'])).toThrow()
 })
 
-describe('Check settings as array', () => {
-	it('If properties have equals items', () => {
-		expect(() => defineOptions({ name: 'Dmitry' }, ['name'])).not.toThrow()
+describe('Plugin settings as array', () => {
+	it('properties have equals options', () => {
+		const object = defineOptions({ name: 'Dmitry' }, ['name'])
+		expect(object).toEqual({ name: 'Dmitry' })
 	})
 
-	it('If properties have more items then settings', () => {
-		expect(() =>
-			defineOptions({ name: 'Dmitry' }, ['name', 'nickname'])
-		).toThrow()
+	it('properties have more options then settings', () => {
+		const fn = () => defineOptions({ name: 'Dmitry' }, ['name', 'nickname'])
+		expect(fn).toThrow()
 	})
 
-	it('If properties have less items then settings', () => {
-		expect(() =>
-			defineOptions({ name: 'Dmitry', nickname: 'dmitrytavern' }, ['name'])
-		).toThrow()
-	})
-})
-
-describe('Check settings as object', () => {
-	it('If properties is correct', () => {
-		expect(() =>
-			defineOptions(
-				{
-					name: 'Dmitry',
-					age: 13,
-					root: false,
-					symbol: Symbol('test'),
-					bigint: BigInt(9007199254740991),
-				},
-				{
-					name: String,
-					age: Number,
-					root: Boolean,
-					symbol: Symbol,
-					bigint: BigInt,
-				}
-			)
-		).not.toThrow()
-	})
-
-	it('If properties is not correct', () => {
-		expect(() => defineOptions({ name: 'Dmitry' }, { name: Number })).toThrow()
-	})
-
-	it('If properties have some types', () => {
-		expect(() =>
-			defineOptions({ age: 13 }, { age: [Number, String] })
-		).not.toThrow()
-	})
-
-	it('If properties have any type', () => {
-		expect(() =>
-			defineOptions({ anyType: undefined }, { anyType: null })
-		).not.toThrow()
+	it('properties have less options then settings', () => {
+		const fn = () => defineOptions({ name: 'Dmitry', age: 13 }, ['name'])
+		expect(fn).toThrow()
 	})
 })
 
-describe('Check property settings as object', () => {
+describe('Plugin settings as object', () => {
+	it('properties types is correct', () => {
+		const object = defineOptions(
+			{
+				name: 'Dmitry',
+				age: 13,
+				root: false,
+			},
+			{
+				name: String,
+				age: Number,
+				root: Boolean,
+			}
+		)
+
+		expect(object).toEqual({
+			name: 'Dmitry',
+			age: 13,
+			root: false,
+		})
+	})
+
+	it('property types is not correct', () => {
+		const fn = () => defineOptions({ name: 'Dmitry' }, { name: Number })
+		expect(fn).toThrow()
+	})
+
+	it('property have some types as array', () => {
+		const object = defineOptions({ age: 13 }, { age: [Number, String] })
+		expect(object).toEqual({ age: 13 })
+	})
+
+	it('property have any type', () => {
+		const object = defineOptions({ anyType: undefined }, { anyType: null })
+		expect(object).toEqual({ anyType: undefined })
+	})
+})
+
+describe('Plugin property settings as object', () => {
 	const opt = { name: 'Dmitry' }
+	const setValidator = (checkValue) => {
+		return { validator: (value) => value === checkValue }
+	}
 
-	it('Object have only type', () => {
+	it('property setting have only type', () => {
 		expect(() => defineOptions(opt, { name: { type: String } })).not.toThrow()
 		expect(() => defineOptions(opt, { name: { type: Number } })).toThrow()
 	})
 
-	it('Object have validator', () => {
-		expect(() =>
-			defineOptions(opt, {
-				name: {
-					type: String,
-					validator: (value) => value === 'Dmitry',
-				},
-			})
-		).not.toThrow()
+	it('property setting have validator', () => {
+		const object = defineOptions(opt, {
+			name: { type: String, ...setValidator('Dmitry') },
+		})
 
-		expect(() =>
+		const fn2 = () =>
 			defineOptions(opt, {
-				name: {
-					type: String,
-					validator: (value) => value === 'Other name',
-				},
+				name: { type: String, ...setValidator('Other name') },
 			})
-		).toThrow()
+
+		expect(object).toEqual(opt)
+		expect(fn2).toThrow()
 	})
 
-	it('Object have required', () => {
-		expect(() =>
+	it('property setting have required setting', () => {
+		const object = defineOptions(
+			{},
+			{ name: { type: String, required: false } }
+		)
+
+		const fn = () =>
+			defineOptions({}, { name: { type: String, required: true } })
+
+		expect(object).toEqual({})
+		expect(fn).toThrow()
+	})
+
+	it('property setting have default setting', () => {
+		const settings = {
+			name: { type: String, default: 'hello world', required: false },
+		}
+
+		const object1 = defineOptions({}, settings)
+		const object2 = defineOptions({ name: 'Dmitry' }, settings)
+
+		expect(object1).toEqual({ name: 'hello world' })
+		expect(object2).toEqual({ name: 'Dmitry' })
+	})
+
+	it('property setting have default setting as funtion', () => {
+		const object = defineOptions(
+			{},
+			{
+				name: {
+					type: Object,
+					required: false,
+					default: () => {
+						return { test: true }
+					},
+				},
+			}
+		)
+
+		expect(object).toEqual({ name: { test: true } })
+	})
+
+	it('property setting have default setting with type error', () => {
+		const fn = () =>
 			defineOptions(
 				{},
 				{
 					name: {
-						type: String,
+						type: Array,
 						required: false,
+						default: () => {
+							return { test: true }
+						},
 					},
 				}
 			)
-		).not.toThrow()
+
+		expect(fn).toThrow()
 	})
 
-	it('Object have default', () => {
-		expect(
-			defineOptions(
-				{},
-				{
-					name: {
-						type: String,
-						default: 'hello world',
-						required: false,
-					},
-				}
-			)
-		).toEqual({ name: 'hello world' })
-
-		expect(
-			defineOptions(
-				{ name: 'Dmitry' },
-				{
-					name: {
-						type: String,
-						default: 'hello world',
-						required: false,
-					},
-				}
-			)
-		).toEqual({ name: 'Dmitry' })
-	})
-
-	it('Object have default as funtion', () => {
-		expect(
+	it('property setting have default setting with validator error', () => {
+		const fn = () =>
 			defineOptions(
 				{},
 				{
@@ -144,9 +158,30 @@ describe('Check property settings as object', () => {
 						default: () => {
 							return { test: true }
 						},
+						validator: (value: { test: boolean }) => {
+							return value.test === false
+						},
 					},
 				}
 			)
-		).toEqual({ name: { test: true } })
+
+		expect(fn).toThrow()
+	})
+
+	it('property setting have not primitive default setting', () => {
+		const object = defineOptions(
+			{},
+			{
+				name: {
+					type: Object,
+					required: false,
+					default: {
+						test: true,
+					},
+				},
+			}
+		)
+
+		expect(object).toEqual({ name: { test: true } })
 	})
 })
