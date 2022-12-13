8d1b20939061a0e81d840003e7cb2e3b66234558
Dmitry Tavern <dogger.work@gmail.com>
2022-12-13 02:52:08 +0200

    feat: add jsdoc comments to schema utilities

--- src/utils/schema.ts
+++ src/utils/schema.ts
@@ -1,25 +1,43 @@
+import { Schema, PropertySchema } from '@types'
 import { metadata } from 'src/utils/metadata'
 
 /**
+ * Returns `true` if the object is a schema, otherwise returns `false`.
+ *
+ * @param object Target object.
  * @public
  */
-export const isSchema = (object: any): boolean =>
-	metadata.get(object, 'isSchema')
+export function isSchema(object: object): object is Schema {
+	return metadata.get(object, 'isSchema')
+}
 
 /**
+ * Returns `true` if the object is a property schema, otherwise returns `false`.
+ *
+ * @param object Target object.
  * @public
  */
-export const isPropertySchema = (object: any): boolean =>
-	metadata.get(object, 'isPropertySchema')
+export function isPropertySchema(object: object): object is PropertySchema {
+	return metadata.get(object, 'isPropertySchema')
+}
 
 /**
+ * Returns `true` if the object is a some schema, otherwise returns `false`.
+ *
+ * @param object Target object.
  * @internal
  */
-export const isHandledSchema = (object: any): boolean =>
-	metadata.get(object, 'isHandledSchema')
+export function isHandledSchema(object: any): boolean {
+	return metadata.get(object, 'isHandledSchema')
+}
 
 /**
+ * Returns `true` if the object is a property schema with an array type,
+ * otherwise returns `false`.
+ *
+ * @param object Target object.
  * @internal
  */
-export const isArrayTypeSchema = (object: any): boolean =>
-	metadata.get(object, 'isArrayType')
+export function isArrayTypeSchema(object: any): boolean {
+	return metadata.get(object, 'isArrayType')
+}
